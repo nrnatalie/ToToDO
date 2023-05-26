2b77821c044ff8b7f663ffcd524d41fdeaa7f6aa
Nathalie Russo <johndoe@example.com>
2023-05-26 14:26:11 +0300

    feat: add  new ArrayList

--- src/Sked.java
+++ src/Sked.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Sked implements Buy { // РАСПИСАНИЕ
 
@@ -31,14 +34,38 @@ public class Sked implements Buy { // РАСПИСАНИЕ
   public int getTime() {
     return time;
   }
-  public void skedAll(){
-    System.out.println("Привет:");
+
+  public void skedAll() {
+    System.out.println("Следующая покупка:");
+  }
+
+  public static final List<String> buys = new ArrayList<>();
+
+  static {
+    buys.add("Платье красное");
+    buys.add("Голубое платье");
+    buys.add("Джинсы");
+    buys.add("Шарф");
+
+    Collections.sort(buys);
+    System.out.println(buys);
+
+
+  }
+
+  @Override
+  public String toString() {
+    return "=================\n" +
+        "name:   " + name + " ";
+
+
   }
 
   @Override
   public void showBuy() {
 
   }
+
 }
 
 
